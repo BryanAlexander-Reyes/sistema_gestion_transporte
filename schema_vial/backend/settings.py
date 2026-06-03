ba070b28fc4274e3105db5f4af0eec29e9c61c9d
BrayanAlexander-Reyes <bryanreyes47k@gmail.com>
2026-06-03 17:23:56 -0500

    se crea versionamiento de la aplicacion

--- schema_vial/backend/settings.py
+++ schema_vial/backend/settings.py
@@ -120,7 +120,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.versioning.UrlPathVersioningPermission',
+    ],
+}
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
--- schema_vial/backend/urls.py
+++ schema_vial/backend/urls.py
@@ -33,7 +33,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/v1/', include('api.urls')),
 
     # Swagger
     path(
